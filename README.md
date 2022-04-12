ccffa16845c5eb565b94dcf8b87eb989961a9c94
Kuba Wojdas <kubaw9@onet.eu>
2022-04-13 01:39:51 +0200

    fixed few bugs, and added readme file

---
+++ README.md
@@ -0,0 +1,21 @@
+## Another small project that I did as part of the recruitment stage.
+
+### This time website is more than just api,because first time I used bootstrap to make it actually look good compared to my last projects.
+
+### REST API DOCUMENTATION
+
+You can actually use query params to filter objects. After rest url (https://api-library-app.herokuapp.com/books/api/) You have to specify <strong>? character</strong> and then params of your choice. You can obviously mix it up but after every single param you have to put <strong>& character</strong>.
+####Example:
+https://api-library-app.herokuapp.com/books/api/?search=hobbit
+
+###Params list:
+<li><strong>search</strong> - used to query any word in title, author name and language</li>
+<li><strong>title</strong> - used to search any word that any title will contain
+<li><strong>pub_date_after</strong> - minimal range of date. You have to provide actual date here like <strong>pub_date_after=2010-01-01</strong>
+<li><strong>pub_date_before</strong> - maximum range of date. You can mix it up with minimal range param
+<li><strong>author</strong> - used to look for word that author name contains. 
+<li><strong>authors</strong> - used to look for direct authors. Important thing is that You have to put <strong>+ character</strong> instead of spaces. You can look for more than just one author. To do this, You have to put <strong>& character</strong> and then specify authors param again like that: <strong>authors=John+Ronald+Reuel+Tolkien&authors=J.+R.+R.+Tolkien</strong>
+<li><strong>languages</strong> - used to look for languages. It can be also used more than once.
+
+##Live site:
+https://api-library-app.herokuapp.com/
--- books_api/api_scraper.py
+++ books_api/api_scraper.py
@@ -15,63 +15,64 @@ def api_data_scraper(values):
         if value:
             url += f'{key}{value}'
     r = requests.get(url).json()
-    for item in r.get('items'):
-        try:
-            volume_data = item.get('volumeInfo')
-            book_obj, created = Book.objects.update_or_create(
-                id=item.get('id'), defaults={
-                    'id': item.get('id'),
-                    'title': volume_data.get('title'),
-                    'pages_number': volume_data.get('pageCount'),
-                    'language': volume_data.get('language')
-                })
+    if r.get('items'):
+        for item in r.get('items'):
+            try:
+                volume_data = item.get('volumeInfo')
+                book_obj, created = Book.objects.update_or_create(
+                    id=item.get('id'), defaults={
+                        'id': item.get('id'),
+                        'title': volume_data.get('title'),
+                        'pages_number': volume_data.get('pageCount'),
+                        'language': volume_data.get('language')
+                    })
 
-            if authors := volume_data.get('authors'):
-                if not created:
-                    book_obj.authors.clear()
-                for author in authors:
-                    author_obj, created = Author.objects.get_or_create(name=author)
-                    book_obj.authors.add(author_obj)
+                if authors := volume_data.get('authors'):
+                    if not created:
+                        book_obj.authors.clear()
+                    for author in authors:
+                        author_obj, created = Author.objects.get_or_create(name=author)
+                        book_obj.authors.add(author_obj)
 
-            if pub_date := volume_data.get('publishedDate'):
-                dates = ['year', 'month', 'day']
-                pub_date = list(map(int, pub_date.split('-')))
-                pub_date_dict_alt = dict(zip_longest(dates, pub_date))
-                try:
-                    if not all(value for value in pub_date_dict_alt.values()):
-                        raise MultipleObjectsReturned
-                    pub_date_dict = dict(zip(dates, pub_date))
-                    date_obj, created = Date.objects.get_or_create(**pub_date_dict)
-                    book_obj.pub_date = date_obj
-                    book_obj.save()
-                except MultipleObjectsReturned:
-                    pub_date_dict = dict(zip(dates, pub_date))
-                    q = Q()
-                    for key, value in pub_date_dict_alt.items():
-                        if value:
-                            q &= Q(**{key: value})
+                if pub_date := volume_data.get('publishedDate'):
+                    dates = ['year', 'month', 'day']
+                    pub_date = list(map(int, pub_date.split('-')))
+                    pub_date_dict_alt = dict(zip_longest(dates, pub_date))
+                    try:
+                        if not all(value for value in pub_date_dict_alt.values()):
+                            raise MultipleObjectsReturned
+                        pub_date_dict = dict(zip(dates, pub_date))
+                        date_obj, created = Date.objects.get_or_create(**pub_date_dict)
+                        book_obj.pub_date = date_obj
+                        book_obj.save()
+                    except MultipleObjectsReturned:
+                        pub_date_dict = dict(zip(dates, pub_date))
+                        q = Q()
+                        for key, value in pub_date_dict_alt.items():
+                            if value:
+                                q &= Q(**{key: value})
+                            else:
+                                q &= Q(**{f'{key}__isnull': True})
+                        if date_object := Date.objects.filter(q):
+                            date_obj = date_object[0]
+                            book_obj.pub_date = date_obj
+                            book_obj.save()
                         else:
-                            q &= Q(**{f'{key}__isnull': True})
-                    if date_object := Date.objects.filter(q):
-                        date_obj = date_object[0]
-                        book_obj.pub_date = date_obj
-                        book_obj.save()
-                    else:
-                        date_obj = Date.objects.create(**pub_date_dict)
-                        book_obj.pub_date = date_obj
-                        book_obj.save()
+                            date_obj = Date.objects.create(**pub_date_dict)
+                            book_obj.pub_date = date_obj
+                            book_obj.save()
 
-            if identifiers := volume_data.get('industryIdentifiers'):
-                for identifier in identifiers:
-                    if identifier.get('type') == 'ISBN_10':
-                        book_obj.ISBN_10 = identifier.get('identifier')
-                        book_obj.save()
-                    if identifier.get('type') == 'ISBN_13':
-                        book_obj.ISBN_13 = identifier.get('identifier')
-                        book_obj.save()
+                if identifiers := volume_data.get('industryIdentifiers'):
+                    for identifier in identifiers:
+                        if identifier.get('type') == 'ISBN_10':
+                            book_obj.ISBN_10 = identifier.get('identifier')
+                            book_obj.save()
+                        if identifier.get('type') == 'ISBN_13':
+                            book_obj.ISBN_13 = identifier.get('identifier')
+                            book_obj.save()
 
-            if thumbnail := volume_data.get('imageLinks'):
-                book_obj.image = thumbnail.get('thumbnail')
-                book_obj.save()
-        except DataError:
-            pass
+                if thumbnail := volume_data.get('imageLinks'):
+                    book_obj.image = thumbnail.get('thumbnail')
+                    book_obj.save()
+            except DataError:
+                pass
